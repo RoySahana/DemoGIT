d3733944bbd1d0c2654072c4d5a6d2e9ce76454c
anindya <anindya@192.168.0.102>
2019-09-22 10:09:23 +0800

    Test1

--- DemoGIT/src/main/java/com/shop/DemoGitApplication.java
+++ DemoGIT/src/main/java/com/shop/DemoGitApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoGitApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Test");
+		System.out.println("Test1");
 		SpringApplication.run(DemoGitApplication.class, args);
 	}
 
